27cede39dd77458e32adaecb7f9eed5becbff4e4
Kyle Smith <k.d.smith@gmail.com>
2019-03-15 16:16:41 -0500

    adds templates and models, models are incomplete

---
+++ main_app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class User(models.Model):
+    user = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    bio = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'user_id': self.id})
+
+class Locations(models.Model):
+    available_targets = models.
+    all_targets = models.
+    base = models.
+    satellite = models.
+    bases = models.
+
+class Stats(models.Model):
+    money = models.
+    strikes = models.
+    strikes_received = models.
